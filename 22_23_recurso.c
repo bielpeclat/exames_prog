f314b242e824ef8f65601791978d4fc0a9dcfb0b
bielpeclat <bielpeclat1@gmail.com>
2026-06-13 00:06:24 +0100

    comecei 22_23_recurso (ficheiros)

---
+++ 22_23_recurso.c
@@ -0,0 +1,17 @@
+// Gabriel Peclat dos Reis Costa, Eng. Informática
+// 2025140643, ISEC - PT
+#include <stdio.h>
+
+int main() {
+
+    printf("estudasses!\n");
+
+    return 0;
+}
+
+int f_armazem (char *f_texto, char *f_binario) {
+
+    
+
+    return 1;
+}
